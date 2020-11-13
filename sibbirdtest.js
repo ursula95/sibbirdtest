517d7acd4179e874c509f1089e39eb1a5690722f
ursula95 <tatyana2101.kuznetsova@yandex.ru>
2020-11-13 09:52:02 +0700

    add main elements

---
+++ sibbirdtest.js
@@ -0,0 +1,15 @@
+let startButtonn;
+let practiceButton;
+
+function run () {
+   
+}
+
+const main = document.getElementById('main');
+startButtonn = document.createElement('div');
+practiceButton = document.createElement('div');
+startButton.classList.add('startButton');
+practiceButton.classList.add('startButton');
+startButtonn.innerText = 'Начать викторину';
+practiceButton.innerText = 'Тренировка';
+
